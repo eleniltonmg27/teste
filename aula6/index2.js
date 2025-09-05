5e76f9fb466c041e4833076ebf79d6d36e7e8081
eleniltonmg27 <eleniltonmg27@gmail.com>
2025-09-05 20:37:16 -0300

    Novos arquivos

---
+++ aula6/index2.js
@@ -0,0 +1,3 @@
+let nome;
+nome = 'Elenilton';
+console.log(nome);
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Elenilton Silva");
+console.log(15.30, 5, `Elenilton Silva `);
+console.log('Meu nome é "Elenilton" . Estou aprendendo JavaScript às', 19 , 'horas da noite');
